f4eb091258cdcd31db41f5f43f5a9dcd562f116d
Joe <j1powder@yahoo.com>
2023-12-15 08:13:07 -0700

    changes

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,28 +1,31 @@
 import { Component } from '@angular/core';
 import { ServerComponent } from '../server/server.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-servers',
   standalone: true,
-  imports: [ServerComponent],
+  imports: [ServerComponent, FormsModule],
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.css',
 })
 export class ServersComponent {
   allowNewServer = false;
-  serverCreationStatus = 'No server was created'
-
+  serverCreationStatus = 'No server was created';
+  serverName = 'This Server';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
   ngOnInit() {}
-  onCreateServer(){
-    this.serverCreationStatus = 'Server was created'
+  onCreateServer() {
+    this.serverCreationStatus =
+      'Server was created. Name is ' + this.serverName;
   }
 
-  onUpdateServerName(){
-    
+  onUpdateServerName(event: any) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
